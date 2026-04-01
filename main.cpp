3ff316ff220b09bad12d49248f27708f19eeb455
YourName <your@email.com>
2026-04-01 17:07:11 +0900

    junhoo

--- main.cpp
+++ main.cpp
@@ -7,7 +7,20 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
+   if (cnt == M) {
+      for (int i = 0; i < M; i++) {
+         cout << vec[i];
+         if (i < M-1) cout << " ";
+      }
+      cout << "\n";
+      return;
+   }
+   int start = vec.empty() ? 1 : vec.back() + 1;
+   for (int i = start; i <= N; i++) {
+      vec.push_back(i);
+      func(cnt + 1);
+      vec.pop_back();
+   }
 
 
 
